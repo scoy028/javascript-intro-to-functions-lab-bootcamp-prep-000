7166a079b0f7560cd27368663fe9963c72d1137f
Stephanie Coy <scoy028@gmail.com>
2018-08-31 02:34:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if (string === string.toLowerCase()) {
     return "I can\'t hear you!"
-  } else {
-    return "I can hear you!"
+  } else if (string === string.toUpperCase()) {
+    return "YES INDEED!"
   }
 }
